45137b4209643543e14d58061e666719925a160b
Max Tuchin <86924596+maxt86@users.noreply.github.com>
2023-04-13 17:51:05 +0200

    Fix transliteration algorithm

--- src/trans/js/script.js
+++ src/trans/js/script.js
@@ -234,7 +234,7 @@ function toCyrillic(data) {
       if (ln == 'i') {
         
         let lp = prev.toLowerCase()
-        if (latinConsonants.includes(lp)) {
+        if (latinConsonants.includes(lp) && next == ln) {
           res += "'"
         }
         
